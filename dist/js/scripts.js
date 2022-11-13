13c196d9fe720471d7e3b86c8780e2ab6a0b8714
JemsIzHere <james.maranion13@gmail.com>
2022-11-13 18:22:42 +0800

    Git Issue Resolved

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,128 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+var emailArray = [];
+var passwordArray = [];
+
+var loginBox = document.getElementById("login");
+var regBox = document.getElementById("register");
+var forgetBox = document.getElementById("forgot");
+
+var loginTab = document.getElementById("lt");
+var regTab = document.getElementById("rt");
+
+function regTabFun() {
+    event.preventDefault();
+
+    regBox.style.visibility = "visible";
+    loginBox.style.visibility = "hidden";
+    forgetBox.style.visibility = "hidden";
+
+    regTab.style.backgroundColor = "rgb(12, 132, 189)";
+    loginTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
+}
+
+function loginTabFun() {
+    event.preventDefault();
+
+    regBox.style.visibility = "hidden";
+    loginBox.style.visibility = "visible";
+    forgetBox.style.visibility = "hidden";
+
+    loginTab.style.backgroundColor = "rgb(12, 132, 189)";
+    regTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
+}
+
+function forTabFun() {
+    event.preventDefault();
+
+    regBox.style.visibility = "hidden";
+    loginBox.style.visibility = "hidden";
+    forgetBox.style.visibility = "visible";
+
+    regTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
+    loginTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
+
+}
+
+
+function register() {
+    event.preventDefault();
+
+    var email = document.getElementById("re").value;
+    var password = document.getElementById("rp").value;
+    var passwordRetype = document.getElementById("rrp").value;
+
+    if (email == "") {
+        alert("Email required.");
+        return;
+    } else if (password == "") {
+        alert("Password required.");
+        return;
+    } else if (passwordRetype == "") {
+        alert("Password required.");
+        return;
+    } else if (password != passwordRetype) {
+        alert("Password don't match retype your Password.");
+        return;
+    } else if (emailArray.indexOf(email) == -1) {
+        emailArray.push(email);
+        passwordArray.push(password);
+
+        alert(email + "  Thanks for the registration! \nPlease try to login now.");
+
+        document.getElementById("re").value = "";
+        document.getElementById("rp").value = "";
+        document.getElementById("rrp").value = "";
+    } else {
+        alert(email + " is already register.");
+        return;
+    }
+}
+
+function login() {
+    event.preventDefault();
+
+    var email = document.getElementById("se").value;
+    var password = document.getElementById("sp").value;
+
+    var i = emailArray.indexOf(email);
+
+    if (emailArray.indexOf(email) == -1) {
+        if (email == "") {
+            alert("Email required.");
+            return;
+        }
+        alert("Email does not exist.");
+        return;
+    } else if (passwordArray[i] != password) {
+        if (password == "") {
+            alert("Password required.");
+            return;
+        }
+        alert("Password does not match.");
+        return;
+    } else {
+        alert(email + "you're are now logged in.  \n Welcome to the Saffy Website.");
+
+        document.getElementById("se").value = "";
+        document.getElementById("sp").value = "";
+        return;
+    }
+
+}
+
+function forgot() {
+    event.preventDefault();
+
+    var email = document.getElementById("fe").value;
+
+    if (emailArray.indexOf(email) == -1) {
+        if (email == "") {
+            alert("Email required.");
+            return;
+        }
+        alert("Email does not exist.");
+        return;
+    }
+
+    alert("An email has been sent to your email address, please do check it in 24hrs. \n Thanks");
+    document.getElementById("fe").value = "";
+}
